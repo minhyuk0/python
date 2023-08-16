d16de20168eb226e6da80bd08dc04bef9aa68b54
minhyuk0 <141692961+minhyuk0@users.noreply.github.com>
2023-08-16 12:37:19 +0900

    Add files via upload

---
+++ 20230816_IT1-2.py
@@ -0,0 +1,24 @@
+# def factorial(n):
+
+#     output = 1
+
+#     for i in range(1, n + 1):
+#         output *= i
+
+#     return output
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print("5!:", factorial(5))
+    
