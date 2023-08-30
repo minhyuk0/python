a6b4c253b54ec48c7be9e3938ded400cf170b5bc
min-hyuk kang <kls549200@gmail.com>
2023-08-30 11:11:48 +0900

    .

---
+++ 20230823_IT1-2.py
@@ -0,0 +1,45 @@
+# tuple_test = 10, 20, 30, 40
+# print("# 괄호가 없는 튜플의 값과 자료형 출력")
+# print("tuple_tset:", tuple_test)
+# print("type(tuple_test:)", type(tuple_test))
+# print()
+
+# a, b, c = 10, 20, 30
+# print("# 괄호가 없는 튜플을 활요한 할당")
+# print("a:", a)
+# print("b:", b)
+# print("C:", c)
+
+
+# a, b = 10, 20
+
+# print("# 교환 전 값")
+# print("a:", a)
+# print("b:", b)
+# print()
+
+# a, b = b, a
+
+# print("# 교환 후 값")
+# print("a:", a)
+# print("b:", b)
+# print()
+
+
+# def test():
+#     return (10, 20)
+
+# a, b = test()
+
+# print("a:", a)
+# print("b:", b)
+
+# def call_10_times(func):
+#     for i in range(10):
+#         func()
+
+# def print_hello():
+#     print("안녕하세요")
+
+# call_10_times(print_hello)
+
