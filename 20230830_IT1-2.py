47c9923c69d5fda97779b14ec8ed8d2d7da1f8bd
min-hyuk kang <kls549200@gmail.com>
2023-08-30 11:45:15 +0900

    .

---
+++ 20230830_IT1-2.py
@@ -0,0 +1,30 @@
+# 구문 오류 : 프로그램 실행 전에 발생하는 오류
+# SyntaxError : 구문에 문제가있어 실행부터 불가능한경우
+
+# 런타임 오류/예외 : 프로그램 실행중에 발생하는 오류
+# 실행이 되었지만 코딩이 정상적으로 입력된곳까지 실행
+# 그 이후 코딩은 문제가있음
+
+### PDF 11p
+# user_input_a = input("정수 입력> ")
+
+# if user_input_a.isdigit():
+#     number_input_a = int(user_input_a)
+#     print("원의 반지름:", number_input_a)
+#     print("원의 둘레:", 2 * 3.14 * number_input_a)
+#     print("원의 넓이 :", 3.14 * number_input_a * number_input_a)
+
+# else:
+#     print("정수를 입력하지 않았습니다.")
+
+
+### PDF 14p
+
+try:
+    number_input_a = int(input("정수 입력> "))
+    print("원의 반지름:", number_input_a)
+    print("원의 둘레:", 2 * 3.14 * number_input_a)
+    print("원의 넓이:", 3.14 * number_input_a * number_input_a)
+
+except:
+    print("무언가 잘못되었습니다.")
