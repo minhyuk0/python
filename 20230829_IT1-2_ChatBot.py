f698ee3c195d155cf7f62722dca1316b8dcf4f70
min-hyuk kang <kls549200@gmail.com>
2023-09-06 12:09:52 +0900

    .

--- 20230829_IT1-2_ChatBot.py
+++ 20230829_IT1-2_ChatBot.py
@@ -3,9 +3,13 @@ import requests
 import time
 import urllib
 
+from random import *
+
 TOKEN = "6654430104:AAFaDs58Kiktjv2EpjBz86WueBZ5cTMYI4M"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
+word = ["banana","apple","grape"]
+gamelist ={}
 
 def get_url(url):
     response = requests.get(url)
@@ -26,27 +30,58 @@ def get_updates(offset=None):
     js = get_json_from_url(url)
     return js
 
-
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-
-
-def echo_all(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
-
-
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
+
+
+def process_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+
+            if(text == "/hangman"):
+                send_message("행맨 게임을 시작합니다. ",chat)
+                word = choice(word)
+                print("game started woth", chat, "/ Q:", word)
+                gamelist[chat] = [word, False]
+            elif(text == "/stop"):
+                send_message("게임을 중단합니다.", chat)
+            elif(text == "/start"):
+                print("new user", chat)
+                send_message("반갑습니다. 행맨 봇입니다. 게임을 시작하길 원하시면 /hangman 명령어를 입력하세요.")
+            elif(len(gamelist[chat]) > 1 and not gamelist[chat][1]):
+                print(chat,"entered",text)
+                succeed = True
+                feedback = ""
+                for w in gamelist[chat][0]:
+                    if w in text:
+                        feedback += w + ""
+                    else:
+                        feedback += "_ "
+                        succeed = False
+                gamelist[chat][1] = succeed
+                send_message(feedback,chat)
+
+                if succeed:
+                    send_message("맞췄습니다!!", chat)
+                    print(chat, "finished a game")
+                    gamelist[chat] = []
+        except Exception as e:
+            print(e)
+
+
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
 
 
 def send_message(text, chat_id):
@@ -61,7 +96,7 @@ def main():
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
+            process_all(updates)
         time.sleep(0.5)
 
 
--- 20230830_IT1-2.py
+++ 20230830_IT1-2.py
@@ -20,11 +20,11 @@
 
 ### PDF 14p
 
-try:
-    number_input_a = int(input("정수 입력> "))
-    print("원의 반지름:", number_input_a)
-    print("원의 둘레:", 2 * 3.14 * number_input_a)
-    print("원의 넓이:", 3.14 * number_input_a * number_input_a)
+# try:
+#     number_input_a = int(input("정수 입력> "))
+#     print("원의 반지름:", number_input_a)
+#     print("원의 둘레:", 2 * 3.14 * number_input_a)
+#     print("원의 넓이:", 3.14 * number_input_a * number_input_a)
 
-except:
-    print("무언가 잘못되었습니다.")
+# except:
+#     print("무언가 잘못되었습니다.")
